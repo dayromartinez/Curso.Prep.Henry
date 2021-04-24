9582049f03defe3b3433d1922b341570188e748f
Your Name <you@example.com>
2021-04-24 02:11:02 -0500

    Ejercicios extra no completos aún

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,12 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var matriz = [];
+  for (let clave in objeto) {
+    const elemento = objeto[clave];
+    matriz.push([clave, elemento]);
+  }
+  return matriz;
 }
 
 
@@ -18,6 +24,148 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,ii=0,j=0,k=0,l=0,m=0,n=0,ñ=0,o=0,p=0,q=0,r=0,s=0,t=0,u=0,v=0,w=0,x=0,y=0,z=0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "a"){
+      a = a + 1;
+    }else if (string[i] === "b"){
+      b = b + 1;
+    }else if (string[i] === "c"){
+      c = c + 1;
+    }else if (string[i] === "d"){
+      d = d + 1;
+    }else if (string[i] === "e"){
+      e = e + 1;
+    }else if (string[i] === "f"){
+      f = f + 1;
+    }else if (string[i] === "g"){
+      g = g + 1;
+    }else if (string[i] === "h"){
+      h = h + 1;
+    }else if (string[i] === "i"){
+      ii = ii + 1;
+    }else if (string[i] === "j"){
+      j = j + 1;
+    }else if (string[i] === "k"){
+      k = k + 1;
+    }else if (string[i] === "l"){
+      l = l + 1;
+    }else if (string[i] === "m"){
+      m = m + 1;
+    }else if (string[i] === "n"){
+      n = n + 1;
+    }else if (string[i] === "ñ"){
+      ñ = ñ + 1;
+    }else if (string[i] === "o"){
+      o = o + 1;
+    }else if (string[i] === "p"){
+      p = p + 1;
+    }else if (string[i] === "q"){
+      q = q + 1;
+    }else if (string[i] === "r"){
+      r = r + 1;
+    }else if (string[i] === "s"){
+      s = s + 1;
+    }else if (string[i] === "t"){
+      t = t + 1;
+    }else if (string[i] === "u"){
+      u = u + 1;
+    }else if (string[i] === "v"){
+      v = v + 1;
+    }else if (string[i] === "w"){
+      w = w + 1;
+    }else if (string[i] === "x"){
+      x = x + 1;
+    }else if (string[i] === "y"){
+      y = y + 1;
+    }else if (string[i] === "z"){
+      z = z + 1;
+    }
+  }
+  
+  let contadorletras = {};
+  if (a > 0) {
+    contadorletras.a = a;
+  }
+  if (b > 0){
+    contadorletras.b = b;
+  }
+  if (c > 0){
+    contadorletras.c = c;
+  }
+  if (d > 0){
+    contadorletras.d = d;
+  }
+  if (e > 0){
+    contadorletras.e = e;
+  }
+  if (f > 0){
+    contadorletras.f = f;
+  }
+  if (g > 0){
+    contadorletras.g = g;
+  }
+  if (h > 0){
+    contadorletras.h = h;
+  }
+  if (ii > 0){
+    contadorletras.i = ii;
+  }
+  if (j > 0){
+    contadorletras.j = j;
+  }
+  if (k > 0){
+    contadorletras.k = k;
+  }
+  if (l > 0){
+    contadorletras.l = l;
+  }
+  if (m > 0){
+    contadorletras.m = m;
+  }
+  if (n > 0){
+    contadorletras.n = n;
+  }
+  if (ñ > 0){
+    contadorletras.ñ = ñ;
+  }
+  if (o > 0){
+    contadorletras.o = o;
+  }
+  if (p > 0){
+    contadorletras.p = p;
+  }
+  if (q > 0){
+    contadorletras.q = q;
+  }
+  if (r > 0){
+    contadorletras.r = r;
+  }
+  if (s > 0){
+    contadorletras.s = s;
+  }
+  if (t > 0){
+    contadorletras.t = t;
+  }
+  if (u > 0){
+    contadorletras.u = u;
+  }
+  if (v > 0){
+    contadorletras.v = v;
+  }
+  if (w > 0){
+    contadorletras.w = w;
+  }
+  if (x > 0){
+    contadorletras.x = x;
+  }
+  if (y > 0){
+    contadorletras.y = y;
+  }
+  if (z > 0){
+    contadorletras.z = z;
+  }
+  return contadorletras;
 }
 
 
